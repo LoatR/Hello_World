babc8c35d1300610a47bb7976d93b19d257c5163
LoatR <m1105.111.dell@gmail.com>
2018-12-06 23:07:03 +0100

    #comment add some modifications

--- src/main/java/com/helloWorld/HelloServlet.java
+++ src/main/java/com/helloWorld/HelloServlet.java
@@ -10,13 +10,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet("/sayHello")
+@WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
 	private static final Logger LOGGER = Logger.getLogger(HelloServlet.class.getCanonicalName());
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		LOGGER.info("Request GET on HelloServlet");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/plain");
+		response.setStatus(200);
+		response.getWriter().append(sayHello());
 	}
+	
+	public String sayHello() {
+		return "Hello world";
+	}
+	
 }
